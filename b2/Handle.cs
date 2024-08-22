d736fe91873c5db2679bc66bea7dd1913b251cb0
Cong Quang <nguyencongquang02082003@gmail.com>
2024-08-22 14:25:49 +0700

    clearing code lần 1

--- b2/Handle.cs
+++ b2/Handle.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.NetworkInformation;
-
+using System.Linq;
 namespace b2
 {
     public class Handle
@@ -64,80 +63,70 @@ namespace b2
                     Terminal.gI().Print(MenuExit, 3, 4, ConsoleColor.Red);
                     break;
             }
-
             // In tọa độ hiện tại của vector (dùng để debug)
             Terminal.gI().Print($"{vector.x} {vector.y}", 0, 0, ConsoleColor.Green);
         }
+        private void displaySupMenu(ConsoleKeyInfo key, string[] listName, Vector vector)
+        {
+            if (key.Key == ConsoleKey.RightArrow)
+            {
+                while (true)
+                {
+                    ConsoleKeyInfo keyx = Console.ReadKey();
 
+                    if (keyx.Key == ConsoleKey.DownArrow)
+                    {
+                        vectorGV.y = Math.Min(listName.Length - 1, vectorGV.y + 1);
+                    }
+                    else if (keyx.Key == ConsoleKey.UpArrow)
+                    {
+                        vectorGV.y = Math.Max(0, vectorGV.y - 1);
+                    }
+                    else if (keyx.Key == ConsoleKey.LeftArrow || keyx.Key == ConsoleKey.Escape)
+                    {
+                        return;
+                    }
+                    int index = 2;
+                    for (int i = 0; i < listName.Length; i++)
+                    {
+                        var color = (i == vector.y) ? ConsoleColor.Red : ConsoleColor.White;
+                        Terminal.gI().Print(listName[i], Terminal.gI().SizeX / 2 - 30, index++, color);
+                    }
+                    Terminal.gI().Print($"{vector.x} {vector.y}", 0, 0, ConsoleColor.Red); //(dùng để debug)
+                }
+            }
+        }
         private void menuGV(ConsoleKeyInfo key, List<Teacher> Teachers)
         {
             Terminal.gI().Print("Danh sách Giảng viên:", Terminal.gI().SizeX / 2 - 30, 1, ConsoleColor.Green);
             Terminal.gI().Print("Chức Năng", 3, 1, ConsoleColor.Cyan);
             Terminal.gI().Print(MenuHS, 3, 3, ConsoleColor.Green);
             Terminal.gI().Print(MenuExit, 3, 4, ConsoleColor.Green);
-
             int yPosition = 2;
             for (int i = 0; i < Teachers.Count; i++)
             {
                 var color = (i == vectorGV.y) ? ConsoleColor.Red : ConsoleColor.White;
                 Terminal.gI().Print(Teachers[i].Name, Terminal.gI().SizeX / 2 - 30, yPosition++, color);
             }
-
-            while (true)
-            {
-                Terminal.gI().Print(MenuGV, 3, 2, ConsoleColor.Red);
-                ConsoleKeyInfo keyx = Console.ReadKey();
-
-                if (keyx.Key == ConsoleKey.DownArrow)
-                {
-                    vectorGV.y = Math.Min(Teachers.Count - 1, vectorGV.y + 1);
-                }
-                else if (keyx.Key == ConsoleKey.UpArrow)
-                {
-                    vectorGV.y = Math.Max(0, vectorGV.y - 1);
-                }
-                else if (keyx.Key == ConsoleKey.LeftArrow || keyx.Key == ConsoleKey.Escape)
-                {
-                    vectorGV.y = 0;
-                    return;
-                }
-                Terminal.gI().Print("Danh sách Giảng viên:", Terminal.gI().SizeX / 2 - 30, 1, ConsoleColor.Green);
-                Terminal.gI().Print("Chức Năng", 3, 1, ConsoleColor.Cyan);
-                Terminal.gI().Print(MenuGV, 3, 2, ConsoleColor.Green);
-                Terminal.gI().Print(MenuHS, 3, 3, ConsoleColor.Green);
-                Terminal.gI().Print(MenuExit, 3, 4, ConsoleColor.Green);
-
-                yPosition = 2;
-                for (int i = 0; i < Teachers.Count; i++)
-                {
-                    var color = (i == vectorGV.y) ? ConsoleColor.Red : ConsoleColor.White;
-                    Terminal.gI().Print(Teachers[i].Name, Terminal.gI().SizeX / 2 - 30, yPosition++, color);
-                }
-                Terminal.gI().Print($"{vectorGV.x} {vectorGV.y}", 0, 0, ConsoleColor.Red);
-            }
+            displaySupMenu(key, Teachers.Select(t => t.Name).ToArray(), vectorGV);
+            Terminal.gI().Print("Danh sách Giảng viên:", Terminal.gI().SizeX / 2 - 30, 1, ConsoleColor.Green);
+            Terminal.gI().Print("Chức Năng", 3, 1, ConsoleColor.Cyan);
+            Terminal.gI().Print(MenuHS, 3, 3, ConsoleColor.Green);
+            Terminal.gI().Print(MenuExit, 3, 4, ConsoleColor.Green);
         }
-
         private void menuHS(ConsoleKeyInfo key, List<Student> Students)
         {
             Terminal.gI().Print("Danh sách Học sinh:", Terminal.gI().SizeX / 2 - 30, 1, ConsoleColor.Green);
             Terminal.gI().Print("Chức Năng", 3, 1, ConsoleColor.Cyan);
-            if (key.Key == ConsoleKey.S)
-            {
-                vectorHS.y = Math.Min(Students.Count - 1, vectorHS.y + 1);
-            }
-            else if (key.Key == ConsoleKey.W)
-            {
-                vectorHS.y = Math.Max(0, vectorHS.y - 1);
-            }
-
-            // Hiển thị danh sách giảng viên với chỉ mục hiện tại(đổi màu)
+            Terminal.gI().Print(MenuGV, 3, 2, ConsoleColor.Green);
+            Terminal.gI().Print(MenuExit, 3, 4, ConsoleColor.Green);
             int yPosition = 2;
             for (int i = 0; i < Students.Count; i++)
             {
-                var color = (i == vectorHS.y) ? ConsoleColor.Red : ConsoleColor.White;  // Giảng viên đang được chọn có màu đỏ
+                var color = (i == vectorGV.y) ? ConsoleColor.Red : ConsoleColor.White;
                 Terminal.gI().Print(Students[i].Name, Terminal.gI().SizeX / 2 - 30, yPosition++, color);
             }
-            // Hiển thị lại các lựa chọn menu sau khi in danh sách
+            displaySupMenu(key, Students.Select(t => t.Name).ToArray(), vectorGV);
             Terminal.gI().Print(MenuGV, 3, 2, ConsoleColor.Green);
             Terminal.gI().Print(MenuExit, 3, 4, ConsoleColor.Green);
         }
